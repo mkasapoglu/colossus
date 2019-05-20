6f51ff684fadd1a6e8f7148bc3c136420fb7449d
Mustafa <kasapoglumustafa@yahoo.com>
2019-05-20 23:57:36 +0300

    Update settings.py

--- colossus/settings.py
+++ colossus/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'colossus/static'),
 ]
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # ==============================================================================
 # MEDIA FILES SETTINGS
 # ==============================================================================
