dc3c404b63d8e40865a6fdb9674dbc73c0a26d08
Mustafa <kasapoglumustafa@yahoo.com>
2019-05-20 23:30:00 +0300

    Update settings.py

--- colossus/settings.py
+++ colossus/settings.py
@@ -62,7 +62,6 @@ INTERNAL_IPS = [
 # ==============================================================================
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
